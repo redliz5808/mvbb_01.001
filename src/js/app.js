9cf3d03088d86416959be73da90fd11a8a69d970
Elizabeth Franklin <elizabethwfranklin@gmail.com>
2026-06-01 15:46:28 -0400

    Refactor project structure

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,28 @@
-const dancingTickets = document.getElementsByClassName("dancing-ticket");
 const windowWidth = window.innerWidth;
+const navLinks = document.getElementsByClassName("nav-link");
+const dancingTickets = document.getElementsByClassName("dancing-ticket");
+const pageUrl = window.location.pathname;
+
+for (let link of navLinks) {
+    let linkDestination = link.pathname;
+
+    let urlArray = pageUrl.split("/");
+    let destinationArray = linkDestination.split("/");
+    let indexOfPage = destinationArray.indexOf("pages");
+    let emptyIndex = destinationArray.indexOf("");
+
+    if (indexOfPage > -1) {
+        destinationArray.splice(indexOfPage, 1);
+    } else if (emptyIndex > -1) {
+        destinationArray.splice(indexOfPage, 1);
+    }
+
+    for (let pathname of destinationArray) {
+        if (urlArray.includes(pathname) && pathname !== "") {
+            link.classList.add("active");
+        }
+    }
+}
 
 if (windowWidth > 992) {
     setTimeout(() => {
